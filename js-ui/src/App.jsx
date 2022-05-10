2a89f98d624656aafca55d0e618d017115cc9866
Andrey Minin <ewkjf@mail.ru>
2022-05-10 18:35:00 +0400

    Storing messages in arrays using structs

--- js-ui/src/App.jsx
+++ js-ui/src/App.jsx
@@ -4,13 +4,14 @@ import './App.css';
 import abi from './utils/WavePortal.json';
 
 const App = () => {
-	/*
+    /*
   * Just a state variable we use to store our user's public wallet.
   */
-	const [currentAccount, setCurrentAccount] = useState('');
+    const [currentAccount, setCurrentAccount] = useState('');
     const [allWaves, setAllWaves] = useState([]);
+    const [message, setMessage] = useState("");
     const contractAddress = '0xDfc0C997B67efE92D7ED17859a1A54Cd4C0AFD76';
-	const contractABI = abi.abi;
+    const contractABI = abi.abi;
 
     /*
      * Create a method that gets all waves from your contract
@@ -49,125 +50,134 @@ const App = () => {
         }
     }
 
-	const checkIfWalletIsConnected = async () => {
-		try {
-			const { ethereum } = window;
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window;
 
-			if (!ethereum) {
-				console.log('Make sure you have metamask!');
-				return;
-			} else {
-				console.log('We have the ethereum object', ethereum);
-			}
+            if (!ethereum) {
+                console.log('Make sure you have metamask!');
+                return;
+            } else {
+                console.log('We have the ethereum object', ethereum);
+            }
 
-			/*
+            /*
              * Check if we're authorized to access the user's wallet
              */
-			const accounts = await ethereum.request({ method: 'eth_accounts' });
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-			if (accounts.length !== 0) {
-				const account = accounts[0];
-				console.log('Found an authorized account:', account);
-				setCurrentAccount(account);
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log('Found an authorized account:', account);
+                setCurrentAccount(account);
                 await getAllWaves()
-			} else {
-				console.log('No authorized account found');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+            } else {
+                console.log('No authorized account found');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	/**
-	 * Implement your connectWallet method here
-	 */
-	const connectWallet = async () => {
-		try {
-			const { ethereum } = window;
+    /**
+     * Implement your connectWallet method here
+     */
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
 
-			if (!ethereum) {
-				alert('Get MetaMask!');
-				return;
-			}
+            if (!ethereum) {
+                alert('Get MetaMask!');
+                return;
+            }
 
-			const accounts = await ethereum.request({
-				method: 'eth_requestAccounts'
-			});
+            const accounts = await ethereum.request({
+                method: 'eth_requestAccounts'
+            });
 
-			console.log('Connected', accounts[0]);
-			setCurrentAccount(accounts[0]);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+            console.log('Connected', accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	const wave = async (message) => {
-		try {
-			const { ethereum } = window;
+    const changeMessage = async event => {
+        setMessage(event.target.value);
+    }
 
-			if (ethereum) {
-				const provider = new ethers.providers.Web3Provider(ethereum);
-				const signer = provider.getSigner();
-				const wavePortalContract = new ethers.Contract(
-					contractAddress,
-					contractABI,
-					signer
-				);
+    const wave = async event => {
+        event.preventDefault();
+        try {
+            const { ethereum } = window;
 
-				let count = await wavePortalContract.getTotalWaves();
-				console.log('Retrieved total wave count...', count.toNumber());
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const wavePortalContract = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer
+                );
 
-        /*
-        * Execute the actual wave from your smart contract
-        */
-        const waveTxn = await wavePortalContract.wave(message);
-        console.log("Mining...", waveTxn.hash);
+                let count = await wavePortalContract.getTotalWaves();
+                console.log('Retrieved total wave count...', count.toNumber());
 
-        await waveTxn.wait();
-        console.log("Mined -- ", waveTxn.hash);
+                /*
+                * Execute the actual wave from your smart contract
+                */
+                const waveTxn = await wavePortalContract.wave(message);
+                console.log("Mining...", waveTxn.hash);
 
-        count = await wavePortalContract.getTotalWaves();
-        console.log("Retrieved total wave count...", count.toNumber());
-			} else {
-				console.log("Ethereum object doesn't exist!");
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+                await waveTxn.wait();
+                console.log("Mined -- ", waveTxn.hash);
 
-	useEffect(() => {
-		checkIfWalletIsConnected();
-	}, []);
+                count = await wavePortalContract.getTotalWaves();
+                console.log("Retrieved total wave count...", count.toNumber());
+            } else {
+                console.log("Ethereum object doesn't exist!");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	return (
-		<div className="mainContainer">
-			<div className="dataContainer">
-				<div className="header">
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    return (
+        <div className="mainContainer">
+            <div className="dataContainer">
+                <div className="header">
                     👋 Hey there!
                 </div>
 
-				<div className="bio">
-					I am Andrey Minin and I worked on self-driving cars so that's pretty
-					cool right? Connect your Ethereum wallet and wave at me!
-				</div>
 
-                <textarea id="noter-text-area" name="textarea" value={"Please enter some message"}/>
-				{/*<input type={<textarea name="" id="" cols="30" rows="10"></textarea>} onClick={wave}>*/}
-				{/*	Wave at Me*/}
-				{/*</input>*/}
-				<button className="waveButton" onClick={wave}>
-					Wave at Me
-				</button>
+                <div className="bio">
+                    I am Andrey Minin and I worked on self-driving cars so that's pretty
+                    cool right? Connect your Ethereum wallet and wave at me!
+                </div>
 
-				{/*
+                <form onSubmit={wave}>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Please enter your message here"
+                        onChange={changeMessage}
+                    />
+                    <input type="submit" value="Wave at Me" className="waveButton" />
+                </form>
+
+
+                {/*
                 * If there is no currentAccount render this button
                 */}
-				{!currentAccount && (
-					<button className="waveButton" onClick={connectWallet}>
-						Connect Wallet
-					</button>
-				)}
+                {!currentAccount && (
+                    <button className="waveButton" onClick={connectWallet}>
+                        Connect Wallet
+                    </button>
+                )}
 
                 {allWaves.map((wave, index) => {
                     return (
@@ -177,10 +187,9 @@ const App = () => {
                             <div>Message: {wave.message}</div>
                         </div>)
                 })}
-
             </div>
-		</div>
-	);
+        </div>
+    );
 };
 
 export default App;
